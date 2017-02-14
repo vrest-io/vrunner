a80ac1c11580c90add9b07a832e5fc1d27c03770
Ramesh Kumar <ramesh.kumar@optimizory.com>
2017-02-14 16:26:07 +0530

    fail handle

--- index.js
+++ index.js
@@ -1289,6 +1289,7 @@ vRunner.prototype.afterComplete = function(report){
   } else if(this.stopped === true) {
     rmk = 'Execution stopped by user.';
   }
+  if(!VARS.$tr) VARS.$tr = {};
   VARS.$tr.result = {
     totalCount : report.total,
     failedCount : report.failed,
@@ -1298,8 +1299,10 @@ vRunner.prototype.afterComplete = function(report){
     remarks : rmk || '',
     resultLink : (this.instanceURL+'/'+this.projectKey+'/testcase') + '?testRunId='+this.testRunId
   };
-  PSTR_HOOK_RUNNER.currTcIndex = -2;
-  callOneQ(PSTR_HOOK_RUNNER,PSTR_HOOK_COL,function(){});
+  if(PSTR_HOOK_RUNNER){
+    PSTR_HOOK_RUNNER.currTcIndex = -2;
+    callOneQ(PSTR_HOOK_RUNNER,PSTR_HOOK_COL,function(){});
+  }
 };
 
 var getFullName = function(usr){
