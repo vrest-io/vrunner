f83a96920881b16145a31a66afb38c000059e62f
Dheeraj Aggarwal <mail.dheerajaggarwal@gmail.com>
2020-06-04 18:00:44 +0530

    equalArrayIgnoreCase operator added

--- index.js
+++ index.js
@@ -1197,6 +1197,67 @@ var setFinalExpContent = function(er,ar){
   return toSet;
 };
 
+let isEqual = function (value, other) {
+
+  // Get the value type
+  var type = Object.prototype.toString.call(value);
+
+  // If the two objects are not the same type, return false
+  if (type !== Object.prototype.toString.call(other)) return false;
+
+  // If items are not an object or array, return false
+  if (['[object Array]', '[object Object]'].indexOf(type) < 0) return false;
+
+  // Compare the length of the length of the two items
+  var valueLen = type === '[object Array]' ? value.length : Object.keys(value).length;
+  var otherLen = type === '[object Array]' ? other.length : Object.keys(other).length;
+  if (valueLen !== otherLen) return false;
+
+  // Compare two items
+  var compare = function (item1, item2) {
+
+    // Get the object type
+    var itemType = Object.prototype.toString.call(item1);
+
+    // If an object or array, compare recursively
+    if (['[object Array]', '[object Object]'].indexOf(itemType) >= 0) {
+      if (!isEqual(item1, item2)) return false;
+    }
+
+    // Otherwise, do a simple comparison
+    else {
+
+      // If the two items are not the same type, return false
+      if (itemType !== Object.prototype.toString.call(item2)) return false;
+
+      // Else if it's a function, convert to a string and compare
+      // Otherwise, just compare
+      if (itemType === '[object Function]') {
+        if (item1.toString() !== item2.toString()) return false;
+      } else {
+        if (item1 !== item2) return false;
+      }
+
+    }
+  };
+
+  // Compare properties
+  if (type === '[object Array]') {
+    for (var i = 0; i < valueLen; i++) {
+      if (compare(value[i], other[i]) === false) return false;
+    }
+  } else {
+    for (var key in value) {
+      if (value.hasOwnProperty(key)) {
+        if (compare(value[key], other[key]) === false) return false;
+      }
+    }
+  }
+
+  // If nothing failed, return true
+  return true;
+};
+
 var setAssertionUtil = function(meta){
   var typeOpts = util.v_asserts.assertTypeOpts,
     unCamelCase = util.unCamelCase.bind(util),
@@ -1216,7 +1277,38 @@ var setAssertionUtil = function(meta){
   });
   for(var ky in meta.assertTests){
     try {
-      funcMap[ky] = eval('(function(a,b){ try { return '+meta.assertTests[ky]+'; } catch(er){console.log(er);} return false;})');
+      if(ky === "equalArrayIgnoreOrder"){
+        funcMap[ky] = function(a, b){
+          try {
+            b = typeof b === "string" ? JSON.parse(b) : b;
+            if(Array.isArray(a) && Array.isArray(b) && a.length === b.length){
+              let map = {};
+              for(let i = 0; i < a.length; i++){
+                for(let j = 0; j < b.length; j++){
+                  if(!map[j]){
+                    if(a[i] !== null && typeof a[i] === "object" && b[j] !== null && typeof b[j] === "object"){
+                      if(isEqual(a[i], b[j])){
+                        map[j] = true;
+                        break;
+                      }
+                    } else if(a[i] === b[j]){
+                      map[j] = true;
+                      break;
+                    }
+                  }
+                }
+              }
+              return Object.keys(map).length === a.length;
+            } 
+            return false;
+          } catch(err){
+            consolelog(err);
+            return false;
+          }
+        }
+      } else {
+        funcMap[ky] = eval('(function(a,b){ try { return '+meta.assertTests[ky]+'; } catch(er){console.log(er);} return false;})');
+      }
     } catch(el){
     }
   }
